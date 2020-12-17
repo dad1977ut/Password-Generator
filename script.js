5a16387d993133ba0941ccc265a10f8e8e348062
Dameon Davis <dad1977ut@yahoo.com>
2020-12-16 18:27:40 -0700

    added number of caracters to code

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  // get the number of caracters the user want the password to have
+  do {
+    numberOfLetters = prompt("How many caracters do you want in your password. Passwords can only be between 8 and 128 caracters long.");
+  }while (numberOfLetters < 8 || numberOfLetters > 128)
+  console.log(numberOfLetters);
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
