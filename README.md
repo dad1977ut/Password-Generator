2c6cb122c5eeea7d8121e1b4af7372b81b85abe2
Dameon Davis <dad1977ut@yahoo.com>
2020-12-23 01:06:01 -0700

    updated the readme file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Password-Generator
+I want to make an application that generate a password using various types of characters.
+
+* click on button to generate a password.
+* ask what type of characters the user want to use.
+* include upper case letters, low case letters, numbers and special characters as choices.
+* generate the password with the given choices.
+
+![before](assets/start.jpg)
+![after](assets/end.jpg)
--- script.js
+++ script.js
@@ -7,21 +7,23 @@ function randomCharacter(arr) {
   return arr[randomNum];
 }
 function generatePassword() {
-  var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var numArr = ["0","1","2","3","4","5","6","7","8","9"];
-  var specialArr =["!","@","#","$","%","^","&","*","(",")"];
+  var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+  var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+  var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")","\"","'"];
   var noUpper = true;
   var noLower = true;
   var noNum = true;
   var noSpecial = true;
   var remainChar = 4;
-  var storePass =" ";
+  var storePass = " ";
   var randomNumArr = [];
   // get the number of caracters the user want the password to have
   do {
-    var numberOfLetters = prompt("How many characters do you want in your password. Passwords can only be between 8 and 128 caracters long.");
-  }while (numberOfLetters < 8 || numberOfLetters > 128)
+    var numberOfLetters = prompt(
+      "How many characters do you want in your password. Passwords can only be between 8 and 128 caracters long."
+    );
+  } while (numberOfLetters < 8 || numberOfLetters > 128);
   // Ask user if they want any upper case letters in their password
   var wantUpperCaseLetter = confirm("Do you want upper case letters?");
   // Ask user if they want any lower case letters in their password
@@ -29,74 +31,78 @@ function generatePassword() {
   // Ask user if they want any numbers in their password
   var wantNumber = confirm("Do you want numbers?");
   // Ask user if they want any special characters in their password
-  var wantSpecialChar = confirm("Do you want special characters?")
+  var wantSpecialChar = confirm("Do you want special characters?");
   // Make an array of base on the choices user pick
-  if(wantUpperCaseLetter){
+  if (wantUpperCaseLetter) {
     randomNumArr.push(1);
   }
-  if(wantLowerCaseLetter){
+  if (wantLowerCaseLetter) {
     randomNumArr.push(2);
   }
-  if(wantNumber){
+  if (wantNumber) {
     randomNumArr.push(3);
   }
-  if(wantSpecialChar){
+  if (wantSpecialChar) {
     randomNumArr.push(4);
   }
   console.log(randomNumArr);
   // check if user didn't pick any criteria
-  if(wantUpperCaseLetter || wantLowerCaseLetter || wantNumber || wantSpecialChar){
-  // create random characters
-     for (var i = 0 ; i < numberOfLetters - 4 ; i++) {
-       var randomNum = randomNumArr[Math.floor(Math.random() * randomNumArr.length)]
-       if(randomNum === 1){
+  if (
+    wantUpperCaseLetter ||
+    wantLowerCaseLetter ||
+    wantNumber ||
+    wantSpecialChar
+  ) {
+    // create random characters
+    for (var i = 0; i < numberOfLetters - 4; i++) {
+      var randomNum =
+        randomNumArr[Math.floor(Math.random() * randomNumArr.length)];
+      if (randomNum === 1) {
         storePass = storePass + randomCharacter(upperArr);
         noUpper = false;
-       }else if(randomNum === 2){
+      } else if (randomNum === 2) {
         storePass = storePass + randomCharacter(lowerArr);
         noLower = false;
-       }else if(randomNum === 3){
+      } else if (randomNum === 3) {
         storePass = storePass + randomCharacter(numArr);
         noNum = false;
-       }else if(randomNum === 4){
+      } else if (randomNum === 4) {
         storePass = storePass + randomCharacter(specialArr);
         noSpecial = false;
-       }
-
-     }
-     if(wantUpperCaseLetter && noUpper){
+      }
+    }
+    if (wantUpperCaseLetter && noUpper) {
       storePass = storePass + randomCharacter(upperArr);
       remainChar = remainChar - 1;
     }
-    if(wantLowerCaseLetter && noLower){
+    if (wantLowerCaseLetter && noLower) {
       storePass = storePass + randomCharacter(lowerArr);
       remainChar = remainChar - 1;
     }
-    if(wantNumber && noNum){
+    if (wantNumber && noNum) {
       storePass = storePass + randomCharacter(numArr);
       remainChar = remainChar - 1;
     }
-    if(wantSpecialChar && noSpecial){
+    if (wantSpecialChar && noSpecial) {
       storePass = storePass + randomCharacter(specialArr);
       remainChar = remainChar - 1;
     }
-     for (i= 0;remainChar - i > 0; i++) {
-      var randomNum = randomNumArr[Math.floor(Math.random() * randomNumArr.length)]
-      if(randomNum === 1){
-      storePass = storePass + randomCharacter(upperArr);
-      }else if(randomNum === 2){
-       storePass = storePass + randomCharacter(lowerArr);
-      }else if(randomNum === 3){
-       storePass = storePass + randomCharacter(numArr);
-      }else if(randomNum === 4){
-       storePass = storePass + randomCharacter(specialArr);
+    for (var i = 0; remainChar - i > 0; i++) {
+      var randomNum =
+        randomNumArr[Math.floor(Math.random() * randomNumArr.length)];
+      if (randomNum === 1) {
+        storePass = storePass + randomCharacter(upperArr);
+      } else if (randomNum === 2) {
+        storePass = storePass + randomCharacter(lowerArr);
+      } else if (randomNum === 3) {
+        storePass = storePass + randomCharacter(numArr);
+      } else if (randomNum === 4) {
+        storePass = storePass + randomCharacter(specialArr);
       }
-     
     }
     return storePass;
-  }
-  else{
-    alert("Cannot complete task. You needs to select at least one criteria.")
+  } else {
+    alert("Cannot complete task. You needs to select at least one criteria.");
   }
 }
 function writePassword() {
@@ -104,7 +110,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
